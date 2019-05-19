21893b3e4b6a82414eb8a05f6bc7f66b3fbabd38
Iskra B <ibatistapoblete@gmail.com>
2019-05-19 12:35:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,22 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+  attr_accessor :name, :artist_name.
+    @@all = []
 
   def self.all
     @@all
+  end
+
+  def self.create(song)
+    song = self.new
+    song.name = name
+    @@all << song
   end
 
   def save
     self.class.all << self
   end
 
+  def self.destroy_all
+    @@all.clear
+  end
 end
